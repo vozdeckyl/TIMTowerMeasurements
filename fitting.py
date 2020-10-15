a4965c56081a6859d2592d1ca69cfab46a5ad67b
Lubos Vozdecky <lubos.vozdecky@cern.ch>
2020-10-15 11:58:41 +0100

    added fitting

--- fitting.py
+++ fitting.py
@@ -41,9 +41,37 @@ plt.axvline(x=(top_interface-5),linewidth=1, color='yellow')
 
 plt.show()
 
-h1 = ROOT.TH1Dh1("hgaus", "histo from a gaussian", 150, 0,150)
 
-for n in range(0,len(line)-1):
-    h1.SetBinContent(n,line[n])
+c1 = ROOT.TCanvas("c1")
+h1 = ROOT.TH1D("hgaus", "histo from a gaussian",300, 0,300)
 
+for n in range(0,len(line)):
+  h1.SetBinContent(n,line[n])
+
+lowerTowerProfileF = ROOT.TF1('lowerTowerProfileF', 'pol2', 0, (bottom_interface-5))
+sampleProfileF = ROOT.TF1('sampleProfileF', 'pol2', (bottom_interface+5), (top_interface-5))
+upperTowerProfileF = ROOT.TF1('upperTowerProfileF', 'pol2', (top_interface+5), len(line))
+
+h1.Fit(lowerTowerProfileF, '', '', 0, (bottom_interface-5))
+h1.Fit(sampleProfileF, '', '', (bottom_interface+5), (top_interface-5))
+h1.Fit(upperTowerProfileF, '', '', (top_interface+5), len(line))
+
+h1.Draw("hist")
+lowerTowerProfileF.Draw("same")
+sampleProfileF.Draw("same")
+upperTowerProfileF.Draw("same")
+
+c1.Print("histo.png")
+
+slopeLower = lowerTowerProfileF.GetParameter(1)
+slopeSample = sampleProfileF.GetParameter(1)
+slopeUpper = upperTowerProfileF.GetParameter(1)
+
+slopeTowerMean = (slopeUpper+slopeLower)/2
+
+kPb = 35.3
+
+kSample = kPb * (slopeTowerMean/slopeSample)
+
+print(kSample)
 
