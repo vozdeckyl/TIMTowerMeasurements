92af91b68ed980fa10b7244f2171743bf146df96
Lubos Vozdecky <lubos.vozdecky@cern.ch>
2020-10-16 14:30:04 +0100

    added numpy fitting

--- fitting.py
+++ fitting.py
@@ -3,8 +3,8 @@
 import ROOT
 import csv
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+import math
 
 
 #fetch the CSV file
@@ -15,11 +15,12 @@ with open("A40M-000231_box.csv") as csvfile:
     imgList.append(row)
 image = np.array(imgList)
 
-line = np.mean(image, axis=0)[50:-50]
+line = np.mean(image, axis=0)
 
 first_derivative = np.gradient(line)
 second_derivative = np.gradient(first_derivative)
 
+plt.figure(figsize=(200,30))
 plt.plot(line, "o")
 #plt.plot(first_derivative, "o")
 #plt.plot(second_derivative)
@@ -33,19 +34,57 @@ print(top_interface)
 plt.axvline(x=bottom_interface,linewidth=1, color='r')
 plt.axvline(x=top_interface,linewidth=1, color='r')
 
-plt.axvline(x=(bottom_interface+5),linewidth=1, color='yellow')
-plt.axvline(x=(bottom_interface-5),linewidth=1, color='yellow')
+plt.axvline(x=(bottom_interface+10),linewidth=1, color='yellow')
+plt.axvline(x=(bottom_interface-10),linewidth=1, color='yellow')
 
-plt.axvline(x=(top_interface+5),linewidth=1, color='yellow')
-plt.axvline(x=(top_interface-5),linewidth=1, color='yellow')
+plt.axvline(x=(top_interface+10),linewidth=1, color='yellow')
+plt.axvline(x=(top_interface-10),linewidth=1, color='yellow')
 
-plt.show()
+plt.savefig("plot.png")
 
 
+coefs_lower, cov_low = np.polyfit(range(0,80), line[40:120], 2,cov = True)
+error_lower = np.sqrt(np.diag(cov_low))
+
+coefs_sample, cov_sample = np.polyfit(range(0,40), line[135:175], 2,cov = True)
+error_sample = np.sqrt(np.diag(cov_sample))
+
+coefs_upper, cov_upper = np.polyfit(range(0,80), line[180:260], 2,cov = True)
+error_upper = np.sqrt(np.diag(cov_upper))
+
+print("lower \t {} +/- {}".format(coefs_lower[1],error_lower[1]))
+print("sample \t {} +/- {}".format(coefs_sample[1],error_sample[1]))
+print("upper \t {} +/- {}".format(coefs_upper[1],error_upper[1]))
+
+kPb = 35.3
+
+gradient_bottom =  coefs_lower[1]
+
+gradient_sample =  coefs_sample[1]
+
+gradient_top  = coefs_upper[1]
+
+gradient_tower_mean = (gradient_bottom+gradient_top)/2
+
+kSample = kPb * (gradient_tower_mean/gradient_sample)
+
+print("gradient_bottom = {}".format(gradient_bottom))
+print("gradient_sample = {}".format(gradient_sample))
+print("gradient_top = {}".format(gradient_top))
+
+relative_error = math.sqrt((error_lower[1]/coefs_lower[1])**2 + (error_sample[1]/coefs_sample[1])**2)
+
+print(kSample)
+
+print("error {}%".format(relative_error*100))
+
+print(kSample*relative_error)
+
+"""
 c1 = ROOT.TCanvas("c1")
 h1 = ROOT.TH1D("hgaus", "histo from a gaussian",300, 0,300)
 
-for n in range(0,len(line)):
+for n in range(0,len(line)-1):
   h1.SetBinContent(n,line[n])
 
 lowerTowerProfileF = ROOT.TF1('lowerTowerProfileF', 'pol2', 0, (bottom_interface-5))
@@ -54,7 +93,7 @@ upperTowerProfileF = ROOT.TF1('upperTowerProfileF', 'pol2', (top_interface+5), l
 
 h1.Fit(lowerTowerProfileF, '', '', 0, (bottom_interface-5))
 h1.Fit(sampleProfileF, '', '', (bottom_interface+5), (top_interface-5))
-h1.Fit(upperTowerProfileF, '', '', (top_interface+5), len(line))
+h1.Fit(upperTowerProfileF, '', '', (top_interface+5), len(line)-1)
 
 h1.Draw("hist")
 lowerTowerProfileF.Draw("same")
@@ -63,7 +102,7 @@ upperTowerProfileF.Draw("same")
 
 c1.Print("histo.png")
 
-slopeLower = lowerTowerProfileF.GetParameter(1)
+slopeLower = 2*lowerTowerProfileF.GetParameter(1)*  + lowerTowerProfileF.GetParameter(1)
 slopeSample = sampleProfileF.GetParameter(1)
 slopeUpper = upperTowerProfileF.GetParameter(1)
 
@@ -74,4 +113,4 @@ kPb = 35.3
 kSample = kPb * (slopeTowerMean/slopeSample)
 
 print(kSample)
-
+"""
